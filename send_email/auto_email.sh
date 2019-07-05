29398b5acac6ad4929f2a4bfbfc1d80e864976e9
dingjingmaster <dingjing@live.cn>
2019-07-05 11:09:26 +0800

    上线

--- send_email/auto_email.sh
+++ send_email/auto_email.sh
@@ -9,9 +9,9 @@ dates="$2"
 content="$3"
 summary="$4"
 email_send_to1="dingjing_ding@easou.cn"     #丁敬
-#email_send_to2="ruan_zheng@easou.cn"        # 儒安
-#email_send_to3="pm-r@easou.cn"
-#email_send_to4="mcgee_zhang@easou.cn"       # 世侠
+email_send_to2="ruan_zheng@easou.cn"        # 儒安
+email_send_to3="pm-r@easou.cn"
+email_send_to4="mcgee_zhang@easou.cn"       # 世侠
 
 to="${email_send_to1}, ${email_send_to2},${email_send_to3}, ${email_send_to4}"
 
