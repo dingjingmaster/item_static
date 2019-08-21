5ccfa1629b565e45087e00c6561626e886830696
dingjingmaster <dingjing@live.cn>
2019-08-21 17:44:30 +0800

    添加图片

--- sql/earn.sql
+++ sql/earn.sql
@@ -7,7 +7,8 @@ CREATE TABLE IF NOT EXISTS `item_earn` (
     `app` INT(12) DEFAULT 0 NOT NULL                COMMENT '1.宜搜　2.微卷',
     `value_type` INT(12) DEFAULT 0 NOT NULL         COMMENT '1.总收益　2.人均收益',
     `value` VARCHAR(32) DEFAULT 0 NOT NULL          COMMENT '值',
-    `time_stamp` INT(12) DEFAULT 0 NOT NULL         COMMENT '时间戳'
+    `time_stamp` INT(12) DEFAULT 0 NOT NULL         COMMENT '时间戳',
+    PRIMARY KEY (`id`)
 );
 
 
