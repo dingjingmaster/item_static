11fa94d137617bf8b7fb5a64adee92e7411c36e0
dingjingmaster <dingjing@live.cn>
2019-08-23 14:17:23 +0800

    修改

--- send_email/summary.py
+++ send_email/summary.py
@@ -149,7 +149,7 @@ if __name__ == '__main__':
     print y
     plt.plot(time_arr, y)
     plt.xticks(time_arr, time_arr, rotation=45)
-    p.set_ylim(bottom=min(y), top=max(y))
+    p.set_ylim(bottom=int(min(y))-100000, top=int(max(y))+100000)
     for a, b in zip(time_arr, y):
         plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
     plt.xlabel('date')
@@ -161,7 +161,7 @@ if __name__ == '__main__':
     print y
     plt.plot(time_arr, y)
     plt.xticks(time_arr, time_arr, rotation=45)
-    p.set_ylim(bottom=min(y), top=max(y))
+    p.set_ylim(bottom=int(min(y))-20, top=int(max(y))+20)
     for a, b in zip(time_arr, y):
         plt.text(a, b, b, ha='center', va='bottom', fontsize=10)
     plt.xlabel('date')
