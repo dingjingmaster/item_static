a402c20302e0fe34f568467241b0c5a320f9147b
dingjingmaster <dingjing@live.cn>
2019-08-30 11:17:12 +0800

    修改

--- send_email/summary.py
+++ send_email/summary.py
@@ -133,11 +133,11 @@ if __name__ == '__main__':
     print time_arr
 
     # 绘制 宜搜天价值量 图形
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 10))
     p = plt.subplot(121)
     y = plot_pic(time_arr, easou_day_arr)
     plt.xticks(time_arr, time_arr, rotation=45)
-    p.set_ylim(bottom=1000000, top=9000000)
+    p.set_ylim(bottom=1000000, top=12000000)
     plt.plot(time_arr, y)
     print y
     for a, b in zip(time_arr, y):
@@ -238,7 +238,7 @@ if __name__ == '__main__':
     fwe.close()
 
     # 绘制微卷天价值图
-    plt.figure(figsize=(10, 5))
+    plt.figure(figsize=(10, 10))
     p = plt.subplot(121)
     y = plot_pic(time_arr, weijuan_day_arr)
     print y
